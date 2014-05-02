366a3cf7a52d972d848761bd5236db8b7d9d0c36
Kshitij Mittal <kshitij.mittal01@gmail.com>
2014-05-02 13:05:27 +0530

    score for prefix and complete words are added. Stopwords removed

--- database.py
+++ database.py
@@ -24,8 +24,19 @@ class DbInsert(tornado.web.RequestHandler):
 		for j in range (len(word_list)):
 			prefix_str=str('')
 			for i in range (len(word_list[j])):
-				prefix_str=str(prefix_str+word_list[j][i])
-				arr.append(prefix_str)
+				if i == len(word_list[j])-1:
+					prefix_str=str(prefix_str+word_list[j][i])
+					indices = [i for i, x in enumerate(word_list) if x == prefix_str]
+					# logging.info(indices)
+					# logging.info(prefix_str)
+					# logging.info("indices length:" + str(len(indices)))
+					prefix_arr=[prefix_str,str(len(indices))]
+					arr.append(prefix_arr)
+				else:
+					prefix_str=str(prefix_str+word_list[j][i])
+					prefix_arr=[prefix_str,'0']
+					arr.append(prefix_arr)
+		logging.info(arr)
 		return arr
 
 	def insert_in_hsets(self,json_string):
@@ -37,13 +48,17 @@ class DbInsert(tornado.web.RequestHandler):
 
 	def insert_in_zsets(self,arr,key):
 		r=redis.StrictRedis(host='localhost',port=6379,db=2)
+		# logging.info(arr)
+		# logging.info(len(arr))
 		for i in range (len(arr)):
-			r.zadd("search_suggest:prefix:"+str(arr[i]),0,key)
-		logging.info("logging in search_suggest:prefix:"+str(arr[i])+"done")	
+			r.zadd("search_suggest:prefix:"+str(arr[i][0]),arr[i][1],key)
+		# logging.info("logging in search_suggest:prefix:"+str(arr[i])+"done")	
 
 	def post(self):
 		json_string=self.get_argument('json_string')
 		arr=self.prefix_array(json_string)
+		# logging.info("checking array return")		
+		# logging.info(arr)
 		key=self.insert_in_hsets(json_string)
 		self.insert_in_zsets(arr,key)
 
@@ -55,9 +70,10 @@ class DbSearch(tornado.web.RequestHandler):
 		prefix_array_length=len(prefix_array)
 		logging.info(prefix_array_length)
 		for i in range(prefix_array_length):
-		 	r.zinterstore("dest",["search_suggest:prefix:"+str(prefix_array[0]),"search_suggest:prefix:"+str(prefix_array[i])])
+		 	r.zinterstore("dest",["search_suggest:prefix:"+str(prefix_array[0]),"search_suggest:prefix:"+str(prefix_array[i])],aggregate='sum')
 		key_array=r.zrange("dest",0,-1)
-		json_return=r.hmget('search_suggest:terms',key_array)
+		logging.info(key_array)
+		json_return=r.hmget('search_suggest:terms',reversed(key_array))
 		json_return_str=''.join(json_return)
 		key_array_str=''.join(key_array)
 		self.write(json_return_str)
--- templates/search_suggest.js
+++ templates/search_suggest.js
@@ -16,6 +16,7 @@ function search_suggest () {
 
 search_suggest.prototype.search=function(prefix,callback) {
    var serializedData2 ='prefix='+prefix;
+   start = new Date().getTime();
   $.ajax({
     type: 'post',
     url: 'http://localhost:8002/search',
